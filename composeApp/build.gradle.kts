7345f2fa3953be4a82a66b8ffdef49e8d34835bb
Rahmani1988 <rahhmani.mohammadreza@gmail.com>
2026-04-20 07:57:43 +0100

    adding api key and buildConfig

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,19 +1,30 @@
+import com.codingfeline.buildkonfig.compiler.FieldSpec
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
+import kotlin.apply
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.buildkonfig)
 }
 
-kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+val localProperties = Properties().apply {
+    val file = rootProject.file("local.properties")
+    if (file.exists()) {
+        load(file.inputStream())
     }
+}
+
+val apiKey = localProperties.getProperty("NEWS_API_KEY") ?: ""
+
+
+kotlin {
+    jvmToolchain(11)
+    androidTarget()
     
     listOf(
         iosArm64(),
@@ -46,6 +57,15 @@ kotlin {
     }
 }
 
+buildkonfig {
+    packageName = "com.reza.multipress"
+
+    defaultConfigs {
+        buildConfigField(FieldSpec.Type.STRING, "apiKey", apiKey)
+    }
+}
+
+
 android {
     namespace = "com.reza.multipress"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
@@ -76,4 +96,3 @@ android {
 dependencies {
     debugImplementation(libs.compose.uiTooling)
 }
-
